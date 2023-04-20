a3bcd786047e48845a249ef19864cd4dd0b930f9
Daniel Bartolini <djb3149@gmail.com>
2023-04-20 16:30:07 -0400

    initial commit

---
+++ src/main/java/com/algorhythmjavaapi/AlgorhythmJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.algorhythmjavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorhythmJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorhythmJavaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algorhythmjavaapi/AlgorhythmJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algorhythmjavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorhythmJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
