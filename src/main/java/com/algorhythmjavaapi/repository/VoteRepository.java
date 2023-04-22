fe3f659e5281d9b49cd95c428ae000668bfa8ad1
Daniel Bartolini <djb3149@gmail.com>
2023-04-22 15:09:15 -0400

    setup models, interfaces

---
+++ src/main/java/com/algorhythmjavaapi/repository/VoteRepository.java
@@ -0,0 +1,12 @@
+package com.algorhythmjavaapi.repository;
+
+import com.algorhythmjavaapi.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+public interface VoteRepository extends JpaRepository<Vote, Integer> {
+    @Query("SELECT count(*) FROM VOTE v where v.postId = :id")
+    int countVotesByPostId(@Param("id") Integer id);
+}
